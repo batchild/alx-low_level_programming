322da1a9f6bb5d95363bbada7a3acdeeed1e1a1e
batchild <batchayachivan1@gmail.com>
2022-12-15 16:23:32 -0800

    love

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * main -Entry point
- * Description: prints 'Main \n'
+ * Description: prints 'Holberton \n'
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char str[] = "Main\n";
+	char str[] = "Holberton\n";
 	int i = 0;
 
 	while (str[i] != '\0')
