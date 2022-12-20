39795936e1473df696e6be1d51220bbac6ba24cd
batchild <batchayachivan1@gmail.com>
2022-12-20 09:10:53 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch '\n');
+	}
+	putchar('\n');
+	return (0);
+}
