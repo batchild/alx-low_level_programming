950a3cc9856978857b92781f0562ae5eddd0c6a6
batchild <batchayachivan1@gmail.com>
2022-12-20 15:52:33 -0800

    0-positive_or_negat

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
