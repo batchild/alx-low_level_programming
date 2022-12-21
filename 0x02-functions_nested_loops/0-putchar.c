4561dc961b537028d695bfd8ecca9c882098f88e
batchild <batchayachivan1@gmail.com>
2022-12-21 02:01:07 -0800

    0-putcha

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main -Entry point
- * Description: prints 'Holberton \n'
- * Return: Always 0 (success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "Holberton\n";
-	int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (str[i] != '\0')
-	{
-		char c = str[i];
-
-		_putchar(c);
-		i++
-	}
 	return (0);
 }
-
